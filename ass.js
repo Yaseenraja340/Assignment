d421be8f0d89a64b628aeb9aac3166fb47494713
rajayaseen <yaseenraja340@gmail.com>
2023-08-02 10:38:10 -0700

    added both files

---
+++ ass.js
@@ -0,0 +1,24 @@
+// let person="Raja Yaseen";
+//  console.log(`hello ${person}, would you like to learn some pythan today?`)
+//  let person1="Raja Yaseen";
+//   console.log(person1.toLowerCase());
+//  console.log(person1.toUpperCase());
+// console.log(`Albert Einstein once said, “A person who never made a mistake never tried anything new.`)
+//  let famous_person="Albert Einstien" 
+//  let message="A person who never made a mistake never tried anything new"
+//    console.log(`${famous_person}:"${message}"`)
+// let name1=` Raja Yaseen `
+//  console.log(name1)
+//  console.log(name1.trim())
+// let addition=5+3
+//  console.log(addition);
+// let subtraction=16-8
+//  console.log(subtraction)
+//  let multiplication=4*2
+//   console.log(multiplication)
+//   let division=16/2
+//    console.log(division)
+console.log(5 + 3);
+console.log(16 - 8);
+console.log(4 * 2);
+console.log(16 / 2);
